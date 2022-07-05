025d1375e9bc6b8a509611d6b02d517db9035af9
eduardo <edp2013.ep@gmail.com>
2022-07-04 21:53:08 -0300

    udapte

---
+++ src/utils/compareTurnoCor.js
@@ -0,0 +1,18 @@
+
+
+
+// const compareTurnoCor = async (element, array) => {
+//   return (
+//     element.length === element.length && // same length and
+//     element.every(
+//       // every element in a
+//       (e1) =>
+//       array.some(
+//           // has a match in b
+//           (e2) => e1.horas === e2.horas && e1.color === e2.color
+//         )
+//     )
+//   );
+// }
+
+// module.exports = compareTurnoCor;
